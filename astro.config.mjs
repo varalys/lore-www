f02d34e6519c6d64f9dae2f5f51ccb2dc6cf317b
franzer <awfulrofl@gmail.com>
2026-01-06 20:06:18 -0700

    Set site URL to lore.varalys.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lore-cli.pages.dev',
+	site: 'https://lore.varalys.com',
 	integrations: [
 		starlight({
 			title: 'Lore',
